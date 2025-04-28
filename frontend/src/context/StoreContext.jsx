53594169b4e373334bddd6b6272338e9c136e957
K.W.D.Udayanga <udayangadeepaka@gmail.com>
2025-04-29 00:02:19 +0530

    Added new files

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,18 +1,49 @@
-import { createContext } from "react";
-import {food_list} from "../../src/assets/assets"
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../../src/assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const contextValue = {
-        food_list
+  const [cartItems, setCartItems] = useState({});
+
+ 
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      if (prev[itemId] > 1) {
+        return { ...prev, [itemId]: prev[itemId] - 1 };
+      } else {
+        const { [itemId]: _, ...rest } = prev; 
+        return rest;
+      }
+    });
+  };
+
+ 
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export default StoreContextProvider;
