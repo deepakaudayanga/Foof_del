35dbe8eff859d1d050a0b42a3a16645572572d60
K.W.D.Udayanga <udayangadeepaka@gmail.com>
2025-05-01 14:13:33 +0530

    Resolved merge conflicts

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import './Cart.css';
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart ,getTotalCartAmount} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -16,54 +18,53 @@ const Cart = () => {
           <p>Total</p>
           <p>Remove</p>
         </div>
-        <br/>
+        <br />
         <hr />
-        {food_list.map((item,index) => {
-          if (cartItems[item._id]>0) {
-            
+        {food_list.map((item) => {
+          if (cartItems[item._id] > 0) {
             return (
-              <div>
-              <div className="cart-items-title cart-items-items" >
-                <img src={item.image} alt=''/>
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>${item.price*cartItems[item._id]}</p>
-                <p onClick={()=> removeFromCart(item._id)} className='cross'>x</p>
-              </div>
-              <hr/>
+              <div key={item._id}>
+                <div className="cart-items-title cart-items-items">
+                  <img src={item.image} alt='' />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${(item.price * cartItems[item._id]).toFixed(2)}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                </div>
+                <hr />
               </div>
             );
           }
-          
         })}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
-                 <p>Subtotal</p>
-                 <p>{getTotalCartAmount()}</p>
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount().toFixed(2)}</p>
             </div>
-            <hr/>
+            <hr />
             <div className="cart-total-details">
-               <p>Delivery Fee </p>
-               <p>${2}</p>
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
-            <hr/>
+            <hr />
             <div className="cart-total-details">
-               <p>Total</p>
-               <p>{getTotalCartAmount()+2}</p>
+              <p>Total</p>
+              <p>${(getTotalCartAmount()===0?0:getTotalCartAmount() + 2).toFixed(2)}</p>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promcode">
           <div>
-            <p>If you have a promo code , Enter it here</p>
+            <p>If you have a promo code, enter it here</p>
             <div className='cart-promcode-input'>
-              <input type='text' placeholder='promo code'/>
+              <input type='text' placeholder='promo code' />
               <button>Submit</button>
             </div>
           </div>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <div>
-      
+    <div className='palce-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Infomation</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='Frist Name' />
+          <input type="text" placeholder='Last Name' />
+        </div>
+        <input type="email" placeholder='Email addres' />
+        <input type="text" placeholder='Street' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zipcode' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+          <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount().toFixed(2)}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${(getTotalCartAmount()===0?0:getTotalCartAmount() + 2).toFixed(2)}</p>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+
     </div>
   )
 }
